574931d08ad2a1fa34a284ceca80c4b07c5ab190
Rustam <189756814+rustleium@users.noreply.github.com>
2025-03-02 08:42:06 +0530

    feat: add 'published' property to Book

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const myLibrary = [];
 
-function Book(title, author, pages, readStatus) {
+function Book(title, author, pages, published, readStatus) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.published = published;
   this.readStatus = readStatus;
 }
 
-function addBookToLibrary(title, author, pages, readStatus) {
-  const newBook = new Book(title, author, pages, readStatus);
+function addBookToLibrary(title, author, pages, published, readStatus) {
+  const newBook = new Book(title, author, pages, published, readStatus);
   myLibrary.push(newBook);
 }
 
-addBookToLibrary('Demian', "Hermann Hesse", 175, "Not Read");
+addBookToLibrary('Demian', "Hermann Hesse", 175, 1919, "Not Read");
