44c383302274ab1d880dd6675f6b5dde3f469bd2
Rustam <189756814+rustleium@users.noreply.github.com>
2025-03-02 09:48:03 +0530

    feat: add function to display books in table

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ function addBookToLibrary(title, author, pages, published, readStatus) {
 }
 
 addBookToLibrary('Demian', "Hermann Hesse", 175, 1919, "Not Read");
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    const tableBody = document.querySelector('.book-list');
+    const bookRow = document.createElement("tr");
+    bookRow.innerHTML =
+      `
+        <td>${book.cover}</td>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td>${book.published}</td>
+        <td>${book.readStatus}</td>
+      `
+    tableBody.appendChild(bookRow);
+
+  });
+}
+
+displayBooks();
